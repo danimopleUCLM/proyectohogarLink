a2dafe3ae8395863b69f47e1444b65807c387fd2
Daniel Moreno Pleite <Daniel.Moreno11@alu.uclm.es>
2025-09-24 16:37:06 +0200

    primer commit

---
+++ src/main/java/es/proyecto/proyectohogarLink/ProyectohogarLinkApplication.java
@@ -0,0 +1,13 @@
+package es.proyecto.proyectohogarLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectohogarLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectohogarLinkApplication.class, args);
+	}
+
+}
