71917a889807e381bb8c1a6e38d68508e4bc67ed
Daniel Moreno Pleite <Daniel.Moreno11@alu.uclm.es>
2025-09-29 17:42:55 +0200

    prueba greeting

--- src/main/java/es/proyecto/proyectohogarLink/controller/WebController.java
+++ src/main/java/es/proyecto/proyectohogarLink/controller/WebController.java
@@ -2,6 +2,10 @@ package es.proyecto.proyectohogarLink.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import es.proyecto.proyectohogarLink.entity.*;
 
 @Controller
 public class WebController {
@@ -11,5 +15,16 @@ public class WebController {
         // Esto le dice a Spring que busque un archivo llamado "inicio.html"
         // en la carpeta de plantillas (templates).
         return "inicio";
+    }
+    @GetMapping("/greeting")
+    public String verPaginaGreeting(Model model) {
+    	model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+    
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+    model.addAttribute("greeting", greeting);
+    return "result";
     }
 }
---
+++ src/main/java/es/proyecto/proyectohogarLink/entity/Greeting.java
@@ -0,0 +1,20 @@
+package es.proyecto.proyectohogarLink.entity;
+
+public class Greeting {
+	private String person;
+	private String content;
+	
+	public String getPerson() {
+	return person;
+	}
+	public void setPerson(String person) {
+	this.person = person;
+	}
+	public String getContent() {
+	return content;
+	}
+	public void setContent(String content) {
+	this.content = content;
+	}
+
+}
