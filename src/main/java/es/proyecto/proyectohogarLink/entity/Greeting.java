4f96a515e16a064f4b9176f257b27bc85d85f8f7
Enrique <enrique@example.com>
2025-10-22 13:53:56 +0200

    Configuración y comprobación de funcionamiento de la Base de Datos (SQLite)

--- src/main/java/es/proyecto/proyectohogarLink/entity/Greeting.java
+++ src/main/java/es/proyecto/proyectohogarLink/entity/Greeting.java
@@ -1,20 +1,27 @@
 package es.proyecto.proyectohogarLink.entity;
 
 public class Greeting {
-	private String person;
-	private String content;
-	
-	public String getPerson() {
-	return person;
-	}
-	public void setPerson(String person) {
-	this.person = person;
-	}
-	public String getContent() {
-	return content;
-	}
-	public void setContent(String content) {
-	this.content = content;
-	}
+    private String name;
+    private String email;
 
+    // Constructor vacío
+    public Greeting() {}
+
+    // Getters y Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
