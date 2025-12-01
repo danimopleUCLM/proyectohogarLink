5d6166ed1f110f6208e955fc42f9081c871986fd
Daniel Moreno Pleite <Daniel.Moreno11@alu.uclm.es>
2025-12-01 15:25:39 +0100

    modificación para quitar del abstractentitydao la gestion manual

--- src/main/java/es/proyecto/proyectohogarLink/DAO/AbstractEntityDAO.java
+++ src/main/java/es/proyecto/proyectohogarLink/DAO/AbstractEntityDAO.java
@@ -1,80 +1,47 @@
 package es.proyecto.proyectohogarLink.DAO;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
 
-/**
- * Clase genérica para operaciones CRUD básicas.
- * T representa la Entidad (Usuario, Inmueble, etc.)
- */
+
 public abstract class AbstractEntityDAO<T> {
 
     protected EntityManager em;
     private Class<T> entityClass;
 
-    // Constructor: Necesitamos el EntityManager y la Clase específica para saber qué buscar
     public AbstractEntityDAO(EntityManager em, Class<T> entityClass) {
         this.em = em;
         this.entityClass = entityClass;
     }
 
     /**
-     * MÉTODO saveEntity (Create)
-     * Guarda un nuevo objeto en la base de datos.
+     * SAVE (CREATE)
+     * Delegamos la transacción a Spring (@Transactional en el Controller)
      */
     public void saveEntity(T entity) {
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            em.persist(entity); // 'persist' es el comando JPA para INSERT
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) tx.rollback();
-            throw new RuntimeException("Error al guardar entidad", e);
-        }
+        em.persist(entity);
     }
 
     /**
-     * MÉTODO selectEntity (Read)
-     * Busca un objeto por su ID.
+     * SELECT (READ)
      */
     public T selectEntity(int id) {
-        // 'find' es el comando JPA para SELECT * FROM tabla WHERE id = ?
         return em.find(entityClass, id);
     }
 
     /**
-     * MÉTODO updateEntity (Update)
-     * Actualiza un objeto existente.
+     * UPDATE
      */
     public void updateEntity(T entity) {
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            em.merge(entity); // 'merge' es el comando JPA para UPDATE
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) tx.rollback();
-            throw new RuntimeException("Error al actualizar entidad", e);
-        }
+        em.merge(entity);
     }
 
     /**
-     * MÉTODO deleteEntity (Delete)
-     * Borra un objeto por su ID.
+     * DELETE
      */
     public void deleteEntity(int id) {
-        EntityTransaction tx = em.getTransaction();
-        try {
-            tx.begin();
-            T entity = em.find(entityClass, id);
-            if (entity != null) {
-                em.remove(entity); // 'remove' es el comando JPA para DELETE
-            }
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) tx.rollback();
-            throw new RuntimeException("Error al borrar entidad", e);
+        T entity = em.find(entityClass, id);
+        if (entity != null) {
+            em.remove(entity);
         }
     }
 }
